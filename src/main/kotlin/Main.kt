e7fc4736ef370a11f0113cdb8d2f6f5e741f9714
muthioo-faith <muthioofaith@gmail.com>
2022-02-12 07:55:35 +0300

    first commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,14 @@
+fun main() {
+    var name = "Faith Mutua"
+    println(name)
+    var age ="21"
+    println(age)
+    var phonenumber = "0702632852"
+    println(phonenumber)
+    var weight = "50kg"
+    println(weight)
+    var iskenyan = true
+    var isnotkenyan = false
+    println(iskenyan)  // output true
+    println(isnotkenyan) // output false
+}
